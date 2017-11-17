cd7fe443b19566c3fc922b740632898ce24ed99c
raph5mrn <raphamim@hotmail.com>
2017-11-17 15:54:22 +0100

    modify htaccess

--- partial/header.php
+++ partial/header.php
@@ -56,6 +56,15 @@
         <script src="js/jquery.fancybox.js"></script>
         <!-- template main js -->
         <script src="js/main.js"></script>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-109821004-1"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-109821004-1');
+        </script>
     </head>
     <body>
         <!--
